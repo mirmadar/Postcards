8bac688487de540d47527c65c070f7f69fb83d19
mirmad <mirmadar@mail.ru>
2024-04-29 01:00:26 +0300

    Возвращаем все на место, кодировку долой

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,3 @@
-FROM ubuntu:16.04
-ENV LANG ru_RU.UTF-8
-RUN apt-get update && \
-    apt-get install -y locales language-pack-ru && \
-    locale-gen ru_RU.UTF-8 && \
-    update-locale LANG=ru_RU.UTF-8 LC_ALL=ru_RU.UTF-8
-ENV LANGUAGE ru_RU.UTF-8
-ENV LC_ALL ru_RU.UTF-8
-
-# Установка Java
 FROM openjdk:20
 WORKDIR /demo
 COPY build/libs/ /demo/
--- database/Schema.sql
+++ database/Schema.sql
@@ -10,10 +10,10 @@ create table services(
     name varchar(255) not null,
     price varchar(15) not null,
     image varchar(255) not null
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+);
 
 create table requests(
     id bigint auto_increment primary key,
     name varchar(255) not null,
     phone_number varchar(15) not null
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+);
