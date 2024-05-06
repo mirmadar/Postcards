02e613aca2b0482a67c0e6facd1fc8f4e70920cb
mirmad <mirmadar@mail.ru>
2024-05-07 00:19:25 +0300

    Для запуска на удаленный сервер готовы

--- src/main/java/com/example/demo/Config/MvcConfig.java
+++ src/main/java/com/example/demo/Config/MvcConfig.java
@@ -1,14 +1,8 @@
 package com.example.demo.Config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
